bca61a4aa2fcc3d796f5e51e5ea657f09c920074
pembo210 <joe@pembo.me>
2014-07-28 01:24:53 -0400

    Simple Python tool to quickly get balances on several BTC accounts at once
    Balance data from Blockchain.info

---
+++ GetBalance.py
@@ -0,0 +1,29 @@
+import urllib, json
+import time
+from Tkinter import *
+
+root = Tk()
+root.wm_title("Blockchain.info Balances")
+text = Text(root)
+scrollbar = Scrollbar(root)
+scrollbar.pack( side = RIGHT, fill=Y )
+
+mylist = Listbox(root, yscrollcommand = scrollbar.set )
+with open('addr.txt', 'r') as f:
+    for line in f:
+		addr = line
+		url = "https://blockchain.info/q/addressbalance/" + addr
+		response = urllib.urlopen(url);
+		data = json.loads(response.read())
+		sdata = format(data / 100000000.0, '.8f')
+		text.insert(INSERT, '\n')	
+		text.insert(INSERT, line)
+		text.insert(INSERT, sdata)
+		text.insert(INSERT, ' BTC')
+		text.insert(END, '\n' )
+
+
+text.pack()
+text.pack( side = LEFT, fill = BOTH )
+scrollbar.config( command = text.yview )
+mainloop()
